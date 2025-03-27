19ab362fc276f5ad204c533727aef86025717e3e
Lefa Mncwanga <pax101gp@outlook.com>
2025-03-27 13:43:56 +0200

    Main Layout and Left Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Homepage />} />
                 <Route
                     path="/sso-callback"
                     element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
                 />
                 <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/chat" element={<ChatPage />} />
+                </Route>
             </Routes>
         </>
     );
